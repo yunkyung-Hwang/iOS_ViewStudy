5169f0589f4e5ea1905bee040420b98398efe18d
Hwang <dudhkd98@naver.com>
2022-01-22 23:55:34 +0900

    TransformViewToImage - save image at gallery

--- iOS_ViewStudy/TransformViewToImage/TransformViewToImageVC.swift
+++ iOS_ViewStudy/TransformViewToImage/TransformViewToImageVC.swift
@@ -4,8 +4,10 @@
 //
 //  Created by 황윤경 on 2022/01/22.
 //  참고: https://ios-development.tistory.com/237
+//  참고: https://ttuk-ttak.tistory.com/85
 
 import UIKit
+import Photos
 
 class TransformViewToImageVC: UIViewController {
 
@@ -15,13 +17,24 @@ class TransformViewToImageVC: UIViewController {
         
     }
     
-    @IBAction func saveViewToImage(_ sender: Any) {
+    @IBAction func shareImage(_ sender: Any) {
         guard let image = baseView.transfromToImage() else {
             return
         }
         let vc = UIActivityViewController(activityItems: [image], applicationActivities: nil)
         vc.excludedActivityTypes = [.saveToCameraRoll]
         present(vc, animated: true)
+    }
+    @IBAction func saveImage(_ sender: Any) {
+        guard let image = baseView.transfromToImage() else {
+            return
+        }
+        PHPhotoLibrary.requestAuthorization { status in
+            guard status == .authorized else { return }
+            PHPhotoLibrary.shared().performChanges({
+                PHAssetChangeRequest.creationRequestForAsset(from: image)
+            }, completionHandler: nil)
+        }
     }
 }
 
