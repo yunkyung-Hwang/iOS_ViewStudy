8df2632c7715ce4bac0a6d611aa21fd790cfec63
Hwang <dudhkd98@naver.com>
2022-01-26 18:12:21 +0900

    ByteCoin - byteCoin apiKey setting and get CoinPriceData form url

--- iOS_ViewStudy/ByteCoin/Model/CoinManager.swift
+++ iOS_ViewStudy/ByteCoin/Model/CoinManager.swift
@@ -10,11 +10,25 @@ import Foundation
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = "7E1F68CC-BBFA-4A48-9496-296ECE134D67"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
     func getCoinPrice(for currency: String) {
-        print(currency)
+        let urlString = "\(baseURL)/\(currency)?apikey=\(apiKey)"
+        
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                let dataAsString = String(data: data!, encoding: .utf8)
+                print(dataAsString)
+            }
+            task.resume()
+        }
     }
 }
