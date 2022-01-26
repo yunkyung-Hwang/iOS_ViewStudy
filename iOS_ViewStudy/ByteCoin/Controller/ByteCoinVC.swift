2abeb535744be6574f85821db42e05e12417d678
Hwang <dudhkd98@naver.com>
2022-01-26 14:57:53 +0900

    ByteCoin - CoinManager의 arr을 받아와서 pickerView setting

--- iOS_ViewStudy/ByteCoin/Controller/ByteCoinVC.swift
+++ iOS_ViewStudy/ByteCoin/Controller/ByteCoinVC.swift
@@ -13,8 +13,27 @@ class ByteCoinVC: UIViewController {
     @IBOutlet weak var currencyLabel: UILabel!
     @IBOutlet weak var currencyPicker: UIPickerView!
     
+    let coinManager = CoinManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self
+    }
+}
+extension ByteCoinVC: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+    
+}
+extension ByteCoinVC: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
     }
 }
