c29bda7ea62a696728d47e98e9e922ed53ddd500
Hwang <dudhkd98@naver.com>
2022-01-26 13:43:35 +0900

    ByteCoin - view setting

---
+++ iOS_ViewStudy/ByteCoin/Controller/ByteCoinVC.swift
@@ -0,0 +1,20 @@
+//
+//  ByteCoinVC.swift
+//  iOS_ViewStudy
+//
+//  Created by 황윤경 on 2022/01/26.
+//
+
+import UIKit
+
+class ByteCoinVC: UIViewController {
+
+    @IBOutlet weak var bitcoinLabel: UILabel!
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
