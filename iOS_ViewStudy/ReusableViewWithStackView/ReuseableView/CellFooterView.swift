bce2a2ba1563f86c4c5274734908c26089b8f24d
Hwang <dudhkd98@naver.com>
2022-02-01 21:13:30 +0900

    2.UIView클래스 작성

--- iOS_ViewStudy/ReusableViewWithStackView/ReuseableView/CellFooterView.swift
+++ iOS_ViewStudy/ReusableViewWithStackView/ReuseableView/CellFooterView.swift
@@ -8,13 +8,36 @@
 import UIKit
 
 class CellFooterView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    @IBOutlet var view: UIView!
+    @IBOutlet weak var likeCnt: UILabel!
+    @IBOutlet weak var ChatCnt: UILabel!
+    
+    private static let NIB_NAME = "CellFooterView"
+    
+    // awakeFromNib
+    override func awakeFromNib() {
+        initWithNib()
     }
-    */
-
+    
+    private func initWithNib() {
+        Bundle.main.loadNibNamed(CellFooterView.NIB_NAME, owner: self, options: nil)
+        addSubview(view)
+        setupLayout()
+        setUpView()
+    }
+    private func setupLayout() {
+        NSLayoutConstraint.activate(
+            [
+                view.topAnchor.constraint(equalTo: topAnchor),
+                view.leadingAnchor.constraint(equalTo: leadingAnchor),
+                view.bottomAnchor.constraint(equalTo: bottomAnchor),
+                view.trailingAnchor.constraint(equalTo: trailingAnchor),
+            ]
+        )
+    }
+    
+    private func setUpView() {
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .clear
+    }
 }
