9b84bfe303e2e82ed592626c8243f695eddefd47
Hwang <dudhkd98@naver.com>
2022-02-01 21:11:23 +0900

    1.File's Owner에 UIView클래스 설정

---
+++ iOS_ViewStudy/ReusableViewWithStackView/ReuseableView/CellFooterView.swift
@@ -0,0 +1,20 @@
+//
+//  CellFooterView.swift
+//  iOS_ViewStudy
+//
+//  Created by 황윤경 on 2022/02/01.
+//
+
+import UIKit
+
+class CellFooterView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
