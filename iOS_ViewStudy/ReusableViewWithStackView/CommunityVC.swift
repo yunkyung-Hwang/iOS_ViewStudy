31ee5b415a4497463d66a28229791323693dd1c9
Hwang <dudhkd98@naver.com>
2022-02-01 20:06:59 +0900

    Cell footerView with StackView

---
+++ iOS_ViewStudy/ReusableViewWithStackView/CommunityVC.swift
@@ -0,0 +1,16 @@
+//
+//  CommunityVC.swift
+//  iOS_ViewStudy
+//
+//  Created by 황윤경 on 2022/02/01.
+//
+
+import UIKit
+
+class CommunityVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
