d9c68c0b3ab22320f07e01909f905b400bc683b5
Hwang <dudhkd98@naver.com>
2022-02-01 21:09:22 +0900

    register HeaderView to cell

---
+++ iOS_ViewStudy/ReusableViewWithStackView/CommunityTVC.swift
@@ -0,0 +1,24 @@
+//
+//  CommunityTVC.swift
+//  iOS_ViewStudy
+//
+//  Created by 황윤경 on 2022/02/01.
+//
+
+import UIKit
+
+class CommunityTVC: UITableViewCell {
+    @IBOutlet weak var cellHeaderView: ProfileHeaderView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- iOS_ViewStudy/ReusableViewWithStackView/CommunityVC.swift
+++ iOS_ViewStudy/ReusableViewWithStackView/CommunityVC.swift
@@ -8,9 +8,35 @@
 import UIKit
 
 class CommunityVC: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "Community"
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+}
 
+extension CommunityVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CommunityCell", for: indexPath) as! CommunityTVC
+        
+//        cell.cellHeaderView.profileImg.image = UIImage(named: "person.circle.fill")
+        cell.cellHeaderView.profileImg.image = UIImage(systemName: "person.circle.fill")
+        cell.cellHeaderView.userName.text = "익명의 사용자"
+        return cell
+    }
+}
+
+extension CommunityVC: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 250
     }
 }
