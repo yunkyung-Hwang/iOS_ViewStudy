6e9050587262993d4792dd2260b8e3e12ad4ff04
Hwang <dudhkd98@naver.com>
2022-02-04 23:31:31 +0900

    ButtonCustom - TODO - setAttributeString

--- iOS_ViewStudy/ButtonCustom/ButtonCustomVC.swift
+++ iOS_ViewStudy/ButtonCustom/ButtonCustomVC.swift
@@ -18,5 +18,17 @@ class ButtonCustomVC: UIViewController {
         button.setTitle("", for: .normal)
         button.setImage(image, for: .normal)
         button.tintColor = .systemIndigo
+        
+        button.addTarget(self, action: #selector(didTapButton(_:)), for: .touchUpInside)
+    }
+    
+    @objc func didTapButton(_ button: UIButton) {
+        //TODO: - setTitle과 setAttributedTitle 차이?
+        //TODO: - string과 NSAttributedString 차이?
+        
+//        button.setTitle("changed", for: .normal)
+        
+//        let btnTitle = NSAttributedString(string: "시작하기")
+//        button.setAttributedTitle(btnTitle, for: .normal)
     }
 }
