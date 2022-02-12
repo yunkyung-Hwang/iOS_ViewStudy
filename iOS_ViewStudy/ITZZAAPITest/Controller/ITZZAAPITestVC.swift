d496ecfa687b8ffaf6e7a603f469bbe778370a07
Hwang <dudhkd98@naver.com>
2022-02-12 23:22:18 +0900

    APITest - delegate Design pattern

--- iOS_ViewStudy/ITZZAAPITest/Controller/ITZZAAPITestVC.swift
+++ iOS_ViewStudy/ITZZAAPITest/Controller/ITZZAAPITestVC.swift
@@ -8,10 +8,33 @@
 import UIKit
 
 class ITZZAAPITestVC: UIViewController {
-    let postManager = PostManager()
+    var postManager = PostManager()
+    var postList: [PostModel]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        postManager.delegate = self
         postManager.getPostModel()
+    }
+}
+
+extension ITZZAAPITestVC: PostManagerDelegate {
+    func didUpdatePost(posts: [PostModel]) {
+        postList = posts
+        postList?.forEach({ post in
+            print("userId: ", post.userId ?? 0)
+            print("boardId: ", post.boardId ?? 0)
+            print("categoryName: ", post.categoryName ?? "")
+            print("profileImgURL: ", post.profileImgURL ?? "")
+            print("nickName: ", post.nickName ?? "")
+            print("postTitle: ", post.postTitle ?? "")
+            print("postContent: ", post.postContent ?? "")
+            print("createdAt: ", post.createdAt ?? "")
+            print("imageCnt: ", post.imageCnt ?? 0)
+            print("likeCnt: ", post.likeCnt ?? 0)
+            print("commentCnt: ", post.commentCnt ?? 0)
+            print()
+        })
     }
 }
--- iOS_ViewStudy/ITZZAAPITest/Model/PostManager.swift
+++ iOS_ViewStudy/ITZZAAPITest/Model/PostManager.swift
@@ -7,8 +7,14 @@
 
 import Foundation
 
+// delegate design pattern - reusable
+protocol PostManagerDelegate {
+    func didUpdatePost(posts: [PostModel])
+}
+
 struct PostManager {
     let baseURL = "http://13.125.239.189:3000"
+    var delegate: PostManagerDelegate?
     
     func getPostModel() {
         let urlString = "\(baseURL)/boards"
@@ -20,14 +26,26 @@ struct PostManager {
         
         URLSession.shared.dataTask(with: url) { data, response, error in
             if let data = data {
-                do {
-                    let resArray: [PostModel] = try
-                    JSONDecoder().decode([PostModel].self, from: data)
-                    print(resArray)
-                } catch let error {
-                    print(error)
+                if let posts = self.parseJSON(postData: data) {
+                    self.delegate?.didUpdatePost(posts: posts)
                 }
             }
         }.resume()
     }
+    
+    func parseJSON(postData: Data) -> [PostModel]? {
+        let decoder = JSONDecoder()
+        do {
+            let decodeData: [PostModel] = try decoder.decode([PostModel].self, from: postData)
+            return decodeData
+        } catch {
+            print(error)
+            return nil
+        }
+    }
 }
+// TODO:
+//1. create a url
+//2. create a url session
+//3. give the session a task
+//4. start the task
