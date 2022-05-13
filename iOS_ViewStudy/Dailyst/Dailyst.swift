0221d589e5311cfc423d2301cf32bf0d926985f4
yunkyung-Hwang <dudhkd98@naver.com>
2022-05-13 23:50:46 +0900

    Dailyst 썸네일 변경 구현

--- iOS_ViewStudy/Dailyst/Dailyst.swift
+++ iOS_ViewStudy/Dailyst/Dailyst.swift
@@ -8,6 +8,7 @@
 import UIKit
 import SnapKit
 import Then
+import Photos
 
 class Dailyst: UIViewController {
     private var playerBaseImage = UIImageView()
@@ -93,6 +94,7 @@ class Dailyst: UIViewController {
         configureContentView()
         configureNaviBar()
         configureEmotionStackView()
+        configurePlayer()
         configureURL()
     }
 }
@@ -145,6 +147,45 @@ extension Dailyst {
             }
             emotionStackView.addArrangedSubview(emotionBaseView)
         }
+    }
+    
+    private func configurePlayer() {
+        changeThumbnailBtn.addTarget(self, action: #selector(showGallery), for: .touchUpInside)
+    }
+    
+    @objc func showGallery(){
+        print("asdf")
+        switch PHPhotoLibrary.authorizationStatus() {
+        case .authorized:
+            // 갤러리 권한 존재, 갤러리로 전환
+            openGallery()
+        case .notDetermined:
+            // 갤러리 권한 요청
+            PHPhotoLibrary.requestAuthorization(for: .readWrite) { (status) in }
+        default:
+            // 갤러리 권한 없음, 설정 화면으로 이동
+            let accessConfirmVC = UIAlertController(title: "권한 필요", message: "갤러리 접근 권한이 없습니다. 설정 화면에서 설정해주세요.", preferredStyle: .alert)
+            let goSettings = UIAlertAction(title: "설정으로 이동", style: .default) { (action) in
+                print("go settings")
+                
+                if let appSettings = URL(string: UIApplication.openSettingsURLString) {
+                    UIApplication.shared.open(appSettings, options: [:], completionHandler: nil)
+                }
+            }
+            let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+            accessConfirmVC.addAction(goSettings)
+            accessConfirmVC.addAction(cancel)
+            self.present(accessConfirmVC, animated: true, completion: nil)
+        }
+    }
+    
+    // Gallery Open
+    func openGallery() {
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = true
+        present(imagePicker, animated: true, completion: nil)
     }
     
     private func configureURL() {
@@ -246,6 +287,26 @@ extension Dailyst: UITextViewDelegate {
         }
     }
 }
+
+//MARK: UIImagePickerControllerDelegate, UINavigationControllerDelegate
+extension Dailyst: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        var selectedImg: UIImage? = nil
+                
+        if let possibleImage = info[.editedImage] as? UIImage {
+            selectedImg = possibleImage
+        } else if let possibleImage = info[.originalImage] as? UIImage {
+            selectedImg = possibleImage
+        }
+        
+        playerCD.image = selectedImg
+        playerCD.contentMode = .scaleAspectFill
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+}
+
 
 extension UITextView {
     func setTextViewPlaceholder(_ placeholder: String) {
