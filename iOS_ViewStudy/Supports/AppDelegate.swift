35099ac2903672163bb0110cceb898934f475779
yunkyung-Hwang <dudhkd98@naver.com>
2022-07-01 21:38:45 +0900

    Dailyst - 잠금화면을 위한 AppDelegate 수정

--- iOS_ViewStudy/Supports/AppDelegate.swift
+++ iOS_ViewStudy/Supports/AppDelegate.swift
@@ -28,6 +28,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        let lockVC = LockVC()
+        lockVC.modalPresentationStyle = .fullScreen
+        UIApplication.topViewController(base: window?.rootViewController)?.present(lockVC, animated: true)
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
@@ -36,10 +39,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-        
-//        let lockVC = LockVC()
-//        lockVC.modalPresentationStyle = .fullScreen
-//        window?.rootViewController?.present(lockVC, animated: false, completion: nil)
     }
 
 
